5e2455d298f07030b8563a1255812405f4b51cee
hwakk <x_xhwakk@naver.com>
2022-02-03 23:48:17 +0900

    Feat: 영화 리스트 기능 개발

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+import MovieForm from "./components/MovieForm";
+
+interface MovieSet {
+  id: Date;
+  title: string;
+  year: number;
+}
+
+function App() {
+  const [movies, setMovies] = useState<MovieSet[]>([]);
+
+  const removeMovie = (id: Date) => {
+    setMovies(
+      movies.filter((movie) => {
+        return movie.id !== id;
+      })
+    );
+  };
+
+  const renderMovies = movies.length
+    ? movies.map((movie) => {
+        return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
+      })
+    : "추가된 영화가 없습니다.";
+
+  const addMovie = (movie: MovieSet) => {
+    setMovies([...movies, movie]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Movie List</h1>
+      <MovieForm addMovie={addMovie} />
+      {renderMovies}
+    </div>
+  );
+}
+
+export default App;
